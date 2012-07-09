337f1dbe16ade67fba35c00e7b9de626c3b5902a
user <user@RM680.(none)>
2012-07-09 23:28:07 +0200

    increase release number to 1.0.1

--- khtsimpletext/__init__.py
+++ khtsimpletext/__init__.py
@@ -36,7 +36,7 @@ import ConfigParser
 
 __author__ = 'Benoit HERVIER (Khertan)'
 __email__ = 'khertan@khertan.net'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 class Settings(QObject):
     '''Config object'''
