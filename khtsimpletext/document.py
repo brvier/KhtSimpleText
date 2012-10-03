54e9323eb6352dfcd14bfbfe471c4a2e359884e8
Benoit HERVIER <khertan@khertan.net>
2012-10-03 09:46:41 +0200

    Start of rewrite, change and try

---
+++ khtsimpletext/document.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2011 Benoit HERVIER <khertan@khertan.net>
+# Licenced under GPLv3
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; version 3 only.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+from PySide.QtCore import Slot, QObject, Property, Signal
+
+import threading
+import os
+import markdown2
+import re
+import htmlentitydefs
+try:
+  from pygments import highlight
+  from pygments.formatters import HtmlFormatter
+  from pygments.lexers import get_lexer_for_filename
+  from pygments.util import ClassNotFound
+except:
+  pass
+
+def _stripTags(text):
+    ''' Remove html text formating from a text'''
+    from BeautifulSoup import BeautifulSoup
+    plainText = _unescape(''.join( \
+        BeautifulSoup( \
+            text.replace('<p style=', '<pre style')) \
+            .body(text=True)))
+    if (plainText.startswith('\n')):
+        return plainText[1:]
+    return plainText
+
+def _colorIt(text, filepath):
+    ''' Syntax highlight a text in html'''
+    try:
+        lexer =  get_lexer_for_filename(filepath)
+        if lexer == None:
+            return (text, None)
+        return (highlight(text, lexer, HtmlFormatter(full=True)), None)
+    except ClassNotFound:
+        return (text, None)
+    except Exception, err:
+        return (text, err)
+
+def _unescape(text):
+    ''' Unescape a text '''
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
+
+class Document(QObject):
+    def __init__(self, filepath):
+        QObject.__init__(self,)
+        self._filename = os.path.basename(filepath)
+        self._filepath = os.path.realpath(filepath)
+        self._isdir = os.path.isdir(filepath)
+        self._data = None
+        print 'Debug: filepath: %s' % filepath
+
+    def _get_isdir(self,):
+        return self._isdir
+
+    def _get_filename(self,):
+        return self._filename
+    def _set_filename(self, value):
+        self._filename = value
+        self.onFilename.emit()
+
+    def _get_filepath(self,):
+        return self._filepath
+    def _set_filepath(self, value):
+        self._filepath = value
+        self._set_filename(os.path.basename(value))
+        self.onFilePathChanged.emit()
+
+    def _get_data(self,):
+        return self._data
+    def _set_data(self, value):
+        self._data = value
+        self.onDataChanged.emit()
+
+    @Slot(unicode)
+    def load(self,):
+        ''' Load the document from a path in a thread'''
+        self._set_ready(False)
+        self.thread = threading.Thread(target=self._load)
+        self.thread.start()
+
+    def _load(self, ):
+        ''' Load the document from a path '''
+        import codecs
+
+        try:
+            with codecs.open(self._filepath, 'r','utf_8') as fh:
+                try:
+                    text = fh.read()
+                    if text.find('\0') > 0:
+                        text = text.decode('utf-16')
+                    text, err = _colorIt(text, self._filepath)
+                    if err: raise err
+                    self._set_data(text)
+                    self._set_ready(True)
+                except Exception, err:
+                    print type(err), err
+                    self._set_data('')
+                    self.onError.emit(unicode(err))
+                    self._set_ready(True)
+        except Exception, err:
+            self._set_data('')
+            self._set_ready(True)
+            self.onError.emit(unicode(err))
+            print err
+
+    @Slot(unicode, result=unicode)
+    def recolorIt(self, text):
+        ''' ReHighlight a text '''
+        text, err = _colorIt(_stripTags(text), self._filepath)
+        if not err:
+            self._set_data(text)
+        else:
+            self.onError.emit(unicode(err))
+
+    @Slot(unicode, result=unicode)
+    def previewMarkdown(self, text):
+        ''' Generate a markdown preview'''
+        try:
+            return markdown2.markdown(self._stripTags(text))
+        except:
+            return text
+
+    @Slot(unicode)
+    def write(self, data):
+        ''' Write the document to a file '''
+        data = self._stripTags(data)
+        try:
+            with open(self.filepath, 'wb') as fh:
+                fh.write(data.encode('utf-8'))
+        except Exception, err:
+            print err
+            self.onError.emit(unicode(err))
+
+    def _get_ready(self):
+        return self._ready
+    def _set_ready(self, value):
+        self._ready = value
+        self.onReadyChanged.emit()
+
+    onIsdirChanged = Signal()
+    onFilenameChanged = Signal()
+    onFilepathChanged = Signal()
+    onDataChanged = Signal()
+    isdir = Property(bool, _get_isdir, notify=onIsdirChanged)
+    filename = Property(unicode, _get_filename,
+                        _set_filename, notify=onFilenameChanged)
+    filepath = Property(unicode, _get_filepath,
+                        _set_filepath, notify=onFilepathChanged)
+    data = Property(unicode, _get_data,
+                        _set_data, notify=onDataChanged)
+
+    onError = Signal(unicode)
+    onReadyChanged = Signal()
+    ready = Property(bool, _get_ready, _set_ready, notify=onReadyChanged)
+
---
+++ khtsimpletext/documentsModel.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2011 Benoit HERVIER <khertan@khertan.net>
+# Licenced under GPLv3
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; version 3 only.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+from PySide.QtGui import QApplication
+from PySide.QtCore import QUrl, Slot, QObject, Signal, \
+                          QAbstractListModel, QModelIndex, \
+                          Property
+from PySide import QtDeclarative
+from PySide.QtOpenGL import QGLWidget
+
+import sys
+import os
+import os.path
+
+from document import Document
+
+class DocumentsModel(QAbstractListModel):
+    COLUMNS = ('filename', 'index', 'data', 'filepath', 'isdir', 'ready', 'document')
+
+    def __init__(self, parent=None):
+        self._documents = {}
+        self._currentPath = u'/home/user/'
+        QAbstractListModel.__init__(self)
+        self.setRoleNames(dict(enumerate(DocumentsModel.COLUMNS)))
+        self.parent = parent
+
+    @Slot()
+    def loadDir(self, ):
+        self._documents = [Document(os.path.join(self._currentPath,
+                                                 filename))
+                           for filename in os.listdir(self._currentPath)]
+        self._documents.append(Document(os.path.join(self._currentPath,
+                                                     '..')))
+        self._sortData()
+
+    def _sortData(self,):
+        self._documents.sort(key=lambda document: (document.filename, )
+                                         , reverse=False)
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self._documents)
+
+    def data(self, index, role):
+        if index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('filename'):
+            return self._documents[index.row()].filename
+        elif index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('index'):
+            return index.row()
+        elif index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('data'):
+            return self._documents[index.row()].data
+        elif index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('filepath'):
+            return self._documents[index.row()].filepath
+        elif index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('isdir'):
+            return self._documents[index.row()].isdir
+        elif index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('ready'):
+            return self._documents[index.row()].ready
+        elif index.isValid() \
+            and role == DocumentsModel.COLUMNS.index('document'):
+            return self._documents[index.row()]
+        return None
+
+    @Slot()
+    def reload(self):
+        self.beginResetModel()
+        self.loadDir()
+        self.endResetModel()
+
+    @Slot(int)
+    def duplicate(self, idx):
+        self.beginResetModel()
+        new_document = self._documents[idx].duplicate()
+        self._documents.append(new_document)
+        self._sortData()
+        self.endResetModel()
+
+    @Slot(int)
+    def loadDocument(self, idx):
+        self._documents[idx].load()
+        self.parent.document = self._documents[idx]
+
+    @Slot(int)
+    def remove(self, idx):
+        self.beginResetModel()
+        self._documents[idx].delete()
+        self._documents.remove(self._documents[idx])
+        self._sortData()
+        self.endResetModel()
+
+    def _get_currentpath(self):
+        return self._currentPath
+
+    def _set_currentpath(self, value):
+        self._currentPath = os.path.realpath(value)
+        self.onCurrentpathChanged.emit()
+        self.reload()
+
+
+    onCurrentpathChanged = Signal()
+
+    currentpath = Property(unicode, _get_currentpath,
+                           _set_currentpath, notify=onCurrentpathChanged)
+
